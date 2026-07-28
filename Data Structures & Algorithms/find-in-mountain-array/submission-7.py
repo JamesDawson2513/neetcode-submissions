6ac1c09fe101a22a8dca08688534ee4381a07aa7
JamesDawson2513 <138683126+JamesDawson2513@users.noreply.github.com>
2026-07-28 18:49:02 +0100

    Add: find-in-mountain-array - submission-7

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-7.py
@@ -0,0 +1,36 @@
+class Solution:
+
+    def findAfter(self, target: int, arr: 'MountainArray',l: int, r: int) -> int:
+        while l <= r:
+            mid = (l+r) // 2
+            cur = arr.get(mid)
+            if cur == target:
+                return mid
+            elif cur < target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return -1
+
+
+    def findInMountainArray(self, target: int, arr: 'MountainArray') -> int:
+        l, r = 0, arr.length() - 1
+        performed = False
+        res = -1
+        while l <= r:
+            mid = (l+r)//2
+            cur = arr.get(mid)
+            if mid == arr.length() - 1 or cur < arr.get(mid + 1):
+                if cur == target:
+                    res = mid
+                    break
+                if cur < target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            else:
+                if cur >= target and not performed: 
+                    res = self.findAfter(target, arr, mid, arr.length()-1)
+                    performed = True
+                r = mid - 1
+        return res
