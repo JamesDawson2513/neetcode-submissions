3dfa8842478796c3b804d4aa25b0b82f2615eeea
JamesDawson2513 <138683126+JamesDawson2513@users.noreply.github.com>
2026-07-29 11:05:53 +0100

    Add: minimum-window-with-characters - submission-6

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-6.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        count = {}
+        res = ""
+
+        for char in t:
+            count[char] = count.get(char, 0) + 1
+        running = len(count)
+
+        if len(t) > len(s):
+            return res
+
+        l,r = 0,0
+
+        while r < len(s):
+            if s[r] in count:
+                count[s[r]] -= 1
+                if count[s[r]] == 0:
+                    running -= 1
+            
+            while running == 0:
+                print(count,s[l:r+1], running)
+                if r-l+1 < len(res) or res == "":
+                    res = s[l:r+1]
+                if s[l] in count.keys():
+                    count[s[l]] += 1
+                    if count[s[l]] == 1:
+                        running += 1
+                l += 1
+
+            r += 1
+            
+        return res
