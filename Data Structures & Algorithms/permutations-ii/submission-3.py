c81b2579b3b4ba688041ba5ea13beb3ac5a97c5c
JamesDawson2513 <138683126+JamesDawson2513@users.noreply.github.com>
2026-08-21 15:35:42 +0100

    Add: permutations-ii - submission-3

---
+++ Data Structures & Algorithms/permutations-ii/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+
+        perms = [[]]
+        
+        for i in range(len(nums)):
+            new_perms = []
+            for p in perms:
+                for j in range(len(p) + 1):
+                    if j > 0 and p[j-1] == nums[i]:
+                        break
+                    p_copy = p.copy()
+                    p_copy.insert(j, nums[i])
+                    new_perms.append(p_copy)
+            perms = new_perms
+            print(perms)
+
+        return perms
