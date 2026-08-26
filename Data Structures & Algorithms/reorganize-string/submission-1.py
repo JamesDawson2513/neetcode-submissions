58da8b8f983991fe36ef3f44819d2ae1056fb2be
JamesDawson2513 <138683126+JamesDawson2513@users.noreply.github.com>
2026-08-26 12:48:20 +0100

    Add: reorganize-string - submission-1

---
+++ Data Structures & Algorithms/reorganize-string/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        minHeap = []
+        res = ""
+        count = Counter(s)
+        for char in count:
+            heapq.heappush(minHeap, [-count[char], char])
+        while minHeap:
+            count, char = heapq.heappop(minHeap)
+            res += char
+            if not minHeap:
+                if count != -1:
+                    return ""
+                else: return res
+            count2, char2 = heapq.heappop(minHeap)
+            res += char2
+            if count != -1:
+                heapq.heappush(minHeap, [count + 1, char])
+            if count2 != -1:
+                heapq.heappush(minHeap, [count2 + 1, char2])
+        return res
+
+
+
