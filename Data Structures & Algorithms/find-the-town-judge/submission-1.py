b1f46ce10a5e75a06fe7dbbe6a3cc9556e14b22f
JamesDawson2513 <138683126+JamesDawson2513@users.noreply.github.com>
2026-08-27 11:40:36 +0100

    Add: find-the-town-judge - submission-1

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        trustLog = {} #Stores arrays [num trustees, num trusters]
+        for i in range(len(trust)):
+            trustLog[trust[i][0]] = [trustLog.get(trust[i][0], [0,0])[0] + 1, trustLog.get(trust[i][0], [0,0])[1]]
+            trustLog[trust[i][1]] = [trustLog.get(trust[i][1], [0,0])[0], trustLog.get(trust[i][1], [0,0])[1] + 1]
+        for i in range(n):
+            if trustLog.get(i + 1, [0,0]) == [0, n-1]:
+                return i + 1
+        return -1
+        
+            
