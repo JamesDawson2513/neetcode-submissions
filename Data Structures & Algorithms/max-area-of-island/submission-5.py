34ba1c0e4ea16a330d69922f9e77ca4047c645b8
JamesDawson2513 <138683126+JamesDawson2513@users.noreply.github.com>
2026-08-27 12:17:31 +0100

    Add: max-area-of-island - submission-5

---
+++ Data Structures & Algorithms/max-area-of-island/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        area = 0
+        rows, cols = len(grid), len(grid[0])
+
+        def dfs(i,j) -> int:
+            if i < 0 or j < 0 or i > rows - 1 or j > cols - 1 or grid[i][j] == 0:
+                return 0 
+            grid[i][j] = 0
+            return (1 + dfs(i+1, j) + dfs(i-1, j) + dfs(i, j+1) + dfs(i, j-1))
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    area = max(area, dfs(i,j))
+        return area
