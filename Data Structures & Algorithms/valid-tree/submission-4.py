0905cf8f30541e3696cd6b0864167dc546ce2e23
JamesDawson2513 <138683126+JamesDawson2513@users.noreply.github.com>
2026-08-27 23:03:32 +0100

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n-1:
+            return False
+        adj = defaultdict(list)
+        visited = {0}
+        for a,b in edges:
+            adj[a].append(b)
+            adj[b].append(a)
+        q = collections.deque([0])
+        while q:
+            node = q.popleft()
+            for adjnode in adj[node]:
+                if adjnode not in visited:
+                    visited.add(adjnode)
+                    q.append(adjnode)
+        return len(visited) == n
+        
