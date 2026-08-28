5a061bd0d6ea4c433ce73254ee1b7ff5ee84edd5
JamesDawson2513 <138683126+JamesDawson2513@users.noreply.github.com>
2026-08-28 15:45:10 +0100

    Add: course-schedule-iv - submission-5

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        pres, trackr, posts = defaultdict(set), defaultdict(set),defaultdict(set)
+        for a, b in prerequisites:
+            pres[a].add(b)
+            trackr[a].add(b)
+            posts[b].add(a)
+        q = collections.deque()
+        for i in range(numCourses):
+            if len(pres[i]) == 0:
+                q.append(i)
+        while q: 
+            course = q.popleft()
+            for post in posts[course]:
+                pres[post] |= pres[course]
+                trackr[post].remove(course)
+                if len(trackr[post]) == 0:
+                    q.append(post)
+        res = []
+        for a, b in queries:
+            if b in pres[a]: res.append(True)
+            else: res.append(False)
+        return res
